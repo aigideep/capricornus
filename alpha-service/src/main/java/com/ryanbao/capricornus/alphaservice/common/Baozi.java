8ff70aa6449886aecdfdf27a2ff35d07ee662612
baozi <baozi@email.com>
2019-08-08 14:32:05 +0800

    c

---
+++ alpha-service/src/main/java/com/ryanbao/capricornus/alphaservice/common/Baozi.java
@@ -0,0 +1,20 @@
+package com.ryanbao.capricornus.alphaservice.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author zlyan
+ * @date: 2019/7/4
+ * @time: 11:11
+ * @description:
+ **/
+public class Baozi {
+
+    public Baozi(){
+
+    }
+
+    public static Baozi getInstance(){
+        return new Baozi();
+    }
+}
